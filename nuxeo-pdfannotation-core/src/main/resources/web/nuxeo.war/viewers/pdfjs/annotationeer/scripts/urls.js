e1bb877709cb8edbe293b85275350f92862562a4
Damon Brown <dbrown@nuxeo.com>
2019-01-29 13:54:36 -0500

    Handle 10.10 moves for WebUI

--- nuxeo-pdfannotation-core/src/main/resources/web/nuxeo.war/viewers/pdfjs/annotationeer/scripts/urls.js
+++ nuxeo-pdfannotation-core/src/main/resources/web/nuxeo.war/viewers/pdfjs/annotationeer/scripts/urls.js
@@ -14,27 +14,33 @@ var commentSaveUrl = 'annotationComment';
 
 var printurl = null;
 
-function initURLS(){
-  var urlParams = new URLSearchParams(window.location.search);
-  var file = urlParams.get("file");
-  printurl = file;
-  if (file) {
-	var pieces = file.split("/");
-	var fragment = 'annotation/' + pieces[10];
-	if (pieces[11] === '@preview') {
-      fragment += "?xpath=file:content";
-	} else if (pieces[11] === '@blob') {
-      fragment += "?xpath=";
-      var idx = 12;
-      var sep = "";
-      while (idx < pieces.length && pieces[idx] !== '@preview') {
-    	  fragment += sep + pieces[idx];
-    	  idx += 1;
-    	  sep = "/";
-      }
+function initURLS() {
+	var urlParams = new URLSearchParams(window.location.search);
+	var file = urlParams.get("file");
+	printurl = file;
+	if (file) {
+		var pieces = file.split("/");
+		var fragment = "";
+		if (pieces[4] === 'nxfile') {
+			fragment = 'annotation/' + pieces[6];
+			fragment += "?xpath=file:content";
+		} else {
+			fragment = 'annotation/' + pieces[10];
+		}
+		if (pieces.length >= 11 && pieces[11] === '@blob') {
+			fragment += "?xpath=";
+			var idx = 12;
+			var sep = "";
+			while (idx < pieces.length && pieces[idx] !== '@preview') {
+				fragment += sep + pieces[idx];
+				idx += 1;
+				sep = "/";
+			}
+		}
+		annotationSaveUrl = fragment;
+		annotationDeleteUrl = fragment;
 	}
-    annotationSaveUrl = fragment;
-    annotationDeleteUrl = fragment;
-  }
-  $.get(restUrl + 'me', function(data) { username = data.id; }, 'json');
+	$.get(restUrl + 'me', function(data) {
+		username = data.id;
+	}, 'json');
 }
